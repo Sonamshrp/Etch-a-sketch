45cc1c482e96fcc6b7dc498263628ed0fcb71079
Sonamshrp <sonamsherpa24k@gmail.com>
2023-03-06 16:32:00 +0545

    adding hover effect leaving pixelated trails

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ function createGrid(row,column){
     for(i=1;i<=row;i++){
     const rows = document.createElement('div');
     rows.classList.add('rows');
+    rows.setAttribute('id','cell')
     
     for(j=1;j<=column;j++){
         const columns = document.createElement('div');
         columns.classList.add('columns');
+        columns.setAttribute('id','cell')
         rows.appendChild(columns);
     }
     container.appendChild(rows);
@@ -19,4 +21,7 @@ hover.addEventListener('mouseover',() => {
     
 })
 
-createGrid(10,10)
+createGrid(10,10);
+
+const cells = document.querySelector('#cell');
+console.log(cells)
