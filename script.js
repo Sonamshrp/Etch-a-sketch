e5bd6c06e17a273203672269cb7d333b5a1e1b45
Sonamshrp <sonamsherpa24k@gmail.com>
2023-03-08 20:18:31 +0545

    fixed code to generate new grid after button click

--- script.js
+++ script.js
@@ -14,23 +14,39 @@ function createGrid(row,column){
     }
     container.appendChild(rows);
   }
+
+  const cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => {
+      cell.addEventListener('mouseover', (e) => {
+        e.target.classList.add('clicked');
+    })
+    //we can use forEach for static node(1) . we should use Array.from() for dynamic node(3).
+    //console.log(cell.nodeType) //it checks whether a node is static or dynamic.
+
+    //console.log(typeof cells.length) //it checks whether a nodelist is static or dynamic.
+    //consoles number for static and function for dymnamic.
+  })
+
 }
+
+
 
 const hover = document.querySelector('.hover');
 
 const button = document.querySelector('.button');
-button.addEventListener('click',()=>{
+
+function startNew(){
+  
     let gridNum = +prompt('size of grid:','enter the number');
-    console.log(gridNum);
+    // below code removes the older grid    
+    while (container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
+    // below code creates a new grid after older one is removed
     createGrid(gridNum, gridNum);
-    const cells = document.querySelectorAll('.cell');
-    cells.forEach(cell => {
-      cell.addEventListener('mouseover', (e) => {
-      e.target.classList.add('clicked');
-    })
-  })
-});
 
+};
+   
 
+button.addEventListener('click',startNew)
 
-console.log(cells)
